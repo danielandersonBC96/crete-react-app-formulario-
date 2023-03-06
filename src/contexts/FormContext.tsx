e16f9a54fcd368486b6e88cc49ed3a036d648935
danielandersonBC96 <124640917+danielandersonBC96@users.noreply.github.com>
2023-03-06 16:32:50 -0400

    Update FormContext.tsx

---
+++ src/contexts/FormContext.tsx
@@ -0,0 +1,104 @@
+import React from "react";
+import { createContext,useContext,useReducer  } from "react";
+
+//Context
+
+const FormContext = createContext <ContextType | undefined >( undefined);
+
+type State = {
+    currentStep: number;
+    name:string ;
+    level: 0 | 1 ;
+    email:string;
+    github:string;
+
+}
+
+type ContextType = {
+
+    state: State;
+    dispatch: ( action: Action) => void
+
+}
+
+type Action = {
+
+    type: FormActions;
+    playload: any ;
+}
+
+const initalDate: State = {
+   currentStep:0,
+   name:'',
+   level:0,
+   email:'',
+   github:'',
+
+
+}
+
+// reduce 
+
+enum FormActions {
+    setCurrentStep,
+    setName, 
+    setLevel,
+    setEmail,
+    setGitHub
+
+}
+
+const FormReduce = ( state:State,action:Action ) => {
+
+    switch(action.type ){
+        case FormActions.setCurrentStep:
+            return{...state, currentStep:action.playload}
+            case FormActions.setName:
+            return{...state, name:action.playload}
+            case FormActions.setLevel:
+            return{...state, level:action.playload} 
+            case FormActions.setEmail:
+            return{ ... state,email:action.playload}  
+            case FormActions.setGitHub:
+            return{...state, gitHub:action.playload}
+            
+            default:
+                return state; 
+
+
+    }
+}
+
+//Provider 
+
+const FormProvider = ({ children}) => {
+
+    const [ state, dispacth] = useReducer( FormReduce,initalDate  );
+    const value = {state,dispacth}
+
+    return (
+        <FormContext.Provider value={ value}>
+
+            { children}
+
+        </FormContext.Provider>
+    )
+
+
+}
+
+//Context Hook
+
+const useForm = ()  => {
+    
+    const context = useContext(FormContext);
+
+    if ( context === undefined) {
+
+        throw new Error ( ' useForme precisa ser usado dentro do FormProvider ')
+
+    }
+
+    return context; 
+
+} 
